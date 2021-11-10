4db839504c78fdff914673ad5e0c558b8e5ffa19
DanielD26 <103547416@student.swin.edu.au>
2021-11-10 13:09:53 +1100

    Dice tests complete

--- HeroGameAdvancedTests/HeroTests.cs
+++ HeroGameAdvancedTests/HeroTests.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using HeroGameAdvancedLib;
 
-namespace HeroGameAdvancedTests
-{
-    public class HeroTests
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace HeroGameAdvancedTests {
+    public class HeroTests {
+        /*
+        [Theory]
+        [InlineData(3,1,2)]
+        [InlineData(2,1,1)]
+        [InlineData(1,1,0)]
+        // timesToAttack is greater that the number of Uses should return -1 and not subtract from Uses.
+        [InlineData(1,2,-1)]
+        public void AttackTest(int uses, int timesToAttack, int expected) {
+            Dice d = new Dice(1,6);
 
+            Hero h = new Hero(1, "Poopy", d, uses);
+            h.Attack(timesToAttack);
+            int result = h.Uses;
+
+            Assert.Equal(expected, result);
         }
+        */
     }
 }
